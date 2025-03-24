d0881f729c26068d1d356d94cf44ab6ed724af6b
amercader <amercadero@gmail.com>
2025-03-24 15:20:50 +0100

    Update test workflow

--- ckanext/search/tests/conftest.py
+++ ckanext/search/tests/conftest.py
@@ -15,22 +15,6 @@ def pytest_addoption(parser):
 def search_providers():
     pass
 
-
-def pytest_sessionstart(session):
-    if session.config.option.search_provider:
-        provider = session.config.option.search_provider
-        source = "CLI option"
-    elif config.get("ckan.search.search_backend"):
-        provider = config["ckan.search.search_backend"]
-        source = "config"
-    else:
-        provider = "solr"
-        source = "default"
-
-    print("Using Search provider: %s (%s)" % (provider, source))
-    import ipdb; ipdb.set_trace()
-
-
 def pytest_runtest_setup(item):
 
     if "search_providers" in item.fixturenames and item.config.option.search_provider:
